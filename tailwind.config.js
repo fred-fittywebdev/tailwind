e2149fed0610e2bd96ad8a008b921b4c4ec3c141
fred-fittywebdev <frederic-guerra_student2021@wild.school>
2021-04-24 15:33:15 +0200

    Mise en place d'un environnement de développement avec Symfony et tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    './templates/**/*.html.twig',
+    './assets/**/*.css',
+    './assets/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
